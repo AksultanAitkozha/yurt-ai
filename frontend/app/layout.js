0114190d7281e59f17f902e63ef4ee6f889ae26b
Aksultan <aksultanakn@gmail.com>
2023-08-07 07:28:39 +0600

    analytics

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { Analytics } from '@vercel/analytics/react';
+import { Analytics } from '@vercel/analytics';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
